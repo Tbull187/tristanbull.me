c5934f51a6059c255bfba7acf7b0d21468b0bea0
Tristan Bull <tristan.tbull@gmail.com>
2020-04-30 14:55:29 -0700

    first commit. mvp site

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <header className="header">
+        <div className="name-text animated fadeInLeft">Tristan Bull</div>
+        <div className="role-container">
+          <div className="role-text animated fadeInLeft">front end engineer</div>
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+        <p className="animated fadeIn delay-1s">
+          From scrappy start-ups to big-name tech companies, I use my knowledge of frontend
+          development, service-based web architecture, databases, and cloud computing to
+          craft simple solutions to complex business problems.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <div className="btn-group animated fadeIn delay-1s">
+          <div>
+            <a 
+              className="link italic"
+              href="bull-tristan-resume.pdf"
+              download
+              >
+                Resume
+            </a>
+          </div>
+          <div>
+            <a 
+              className="link italic" 
+              href="mailto:tristan.tbull@gmail.com?subject=Reaching out from tristanbull.me"
+              target="_blank"
+              rel="noopener noreferrer">
+                Contact Me
+            </a>
+          </div>
+        </div>
+
       </header>
+      {/* <div className="content">
+
+      </div> */}
     </div>
   );
 }
