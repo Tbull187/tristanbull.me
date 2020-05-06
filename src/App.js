701093590ff13a1e19d22a17b4bf2a634847478d
Tristan Bull <tristan.tbull@gmail.com>
2020-05-06 12:30:51 -0700

    make header its own component

--- src/App.js
+++ src/App.js
@@ -1,47 +1,14 @@
 import React from 'react';
-import logo from './assets/logo.svg';
+import { Header } from './components/Header';
 import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="header">
-        <div className="name-text animated fadeInLeft">Tristan Bull</div>
-        <div className="role-container">
-          <div className="role-text animated fadeInLeft">front end engineer</div>
-          <img src={logo} className="App-logo" alt="logo" />
-        </div>
-        <p className="blurb animated fadeIn delay-1s">
-          From scrappy start-ups to big-name tech companies, I use my knowledge of frontend
-          development, service-based web architecture, databases, and cloud computing to
-          craft simple solutions to complex business problems.
-        </p>
+      <Header />
+      <div className="content">
 
-        <div className="btn-group animated fadeIn delay-1s">
-          <div className="resume-link">
-            <a 
-              className="italic"
-              href="bull-tristan-resume.pdf"
-              download
-              >
-                Resume
-            </a>
-          </div>
-          <div>
-            <a 
-              className="italic" 
-              href="mailto:tristan.tbull@gmail.com?subject=Reaching out from tristanbull.me"
-              target="_blank"
-              rel="noopener noreferrer">
-                Contact Me
-            </a>
-          </div>
-        </div>
-
-      </header>
-      {/* <div className="content">
-
-      </div> */}
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import logo from '../assets/logo.svg';
+
+export const Header = () => {
+  return (
+    <header className="header">
+      <div className="name-text animated fadeInLeft">Tristan Bull</div>
+      <div className="role-container">
+        <div className="role-text animated fadeInLeft">front end engineer</div>
+        <img src={logo} className="App-logo" alt="logo" />
+      </div>
+      <p className="blurb animated fadeIn delay-1s">
+        From scrappy start-ups to big-name tech companies, I use my knowledge of frontend
+        development, service-based web architecture, databases, and cloud computing to
+        craft simple solutions to complex business problems.
+      </p>
+      <div className="btn-group animated fadeIn delay-1s">
+        <div className="resume-link">
+          <a 
+            className="italic"
+            href="bull-tristan-resume.pdf"
+            download
+            >
+              Resume
+          </a>
+        </div>
+        <div>
+          <a 
+            className="italic" 
+            href="mailto:tristan.tbull@gmail.com?subject=Reaching out from tristanbull.me"
+            target="_blank"
+            rel="noopener noreferrer">
+              Contact Me
+          </a>
+        </div>
+      </div>
+    </header>      
+  )
+}
